c54b0b60cf404206bf240346cf3ee29eb7667439
Raluca-Andreea Petcu <119361889+Raluca2808@users.noreply.github.com>
2024-01-25 01:54:29 +0200

    Commit

--- steps/inventory_steps.py
+++ steps/inventory_steps.py
@@ -22,3 +22,24 @@ def step_impl(context):
 @then('cart: The URL has changed')
 def step_impl(context):
     context.shopping_cart_page.getshopping_cart_URL()
+@When('inventory: I click on the add backpack button')
+def step_impl(context):
+    context.inventory_page.click_add_backpack()
+
+@then('inventory: Remove button is displayed')
+def step_impl(context):
+    context.inventory_page.check_remove_button()
+
+@When('inventory: I click the setting button')
+def step_impl(context):
+    context.inventory_page.click_settings_btn()
+@Then('inventory: I click the logout button')
+def step_impl(context):
+    context.inventory_page.click_logout_btn()
+
+@When('inventory: I click the bikelight button')
+def step_impl(context):
+    context.inventory_page.click_bikelight()
+@Then('inventory: Remove button bike is displayed')
+def step_impl(context):
+    context.inventory_page.check_remove_buttonbike()
